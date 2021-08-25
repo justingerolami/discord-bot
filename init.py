86fdfc7287bb181428342e6d6637c9952e2e8b9b
justingerolami <justin.gerolami@queensu.ca>
2021-08-25 00:53:55 -0400

    Fixed issue with create_engine not closing, added proper db connect function

--- init.py
+++ init.py
@@ -16,6 +16,7 @@ ADMIN = os.getenv('ADMIN')
 url = os.getenv('url')
 
 client = discord.Client()
+db = create_engine(url, echo = False)
 
 # When the bot connects to Discord
 @client.event
@@ -44,10 +45,10 @@ async def on_message(message):
 		username = message.content[8:]
 		try:
 			#connect to the database
-			db = create_engine(url, echo = False)
+			conn = db.connect()
 
 			#query for the joined date and the age of the user
-			result = db.execute("SELECT joined, AGE(joined) FROM members WHERE LOWER(username) = LOWER('{username}')".format(username=username)).fetchone()
+			result = conn.execute("SELECT joined, AGE(joined) FROM members WHERE LOWER(username) = LOWER('{username}')".format(username=username)).fetchone()
 
 			#convert to nice format
 			niceJoinedDate = result[0].strftime('%b %d, %Y')
@@ -65,6 +66,7 @@ async def on_message(message):
 			await message.channel.send('{name}, you\'ve been a member for {days} days. You Joined on {joinedDate}.\n'\
 			 							'Your rank should be {currentRank}!\n'\
 			 							'{rankMsg}'.format(name=username,days=numDays, joinedDate=niceJoinedDate, currentRank=rank, rankMsg=rankMsg))
+			conn.close()
 
 		except Exception as e:
 			await message.channel.send('Username not found. Please enter the name when you applied.')
@@ -83,18 +85,20 @@ async def on_message(message):
 			oldname = result[0]
 			newname = result[1]
 
-			db = create_engine(url, echo = False)
+			conn = db.connect()
 
 			#Query to see if record exists
-			if db.execute("SELECT EXISTS(SELECT 1 FROM members WHERE LOWER(username) = LOWER('{oldname}'))".format(oldname=oldname)).fetchone()[0] == True:
+			if conn.execute("SELECT EXISTS(SELECT 1 FROM members WHERE LOWER(username) = LOWER('{oldname}'))".format(oldname=oldname)).fetchone()[0] == True:
 				#query to update the name
-				result = db.execute("UPDATE members SET username = '{newname}' WHERE LOWER(username) = LOWER('{oldname}')".format(newname=newname, oldname=oldname))
+				result = conn.execute("UPDATE members SET username = '{newname}' WHERE LOWER(username) = LOWER('{oldname}')".format(newname=newname, oldname=oldname))
 				await message.channel.send('Updated username from {oldname} to {newname}'.format(oldname=oldname,newname=newname))
 			else:
 				await message.channel.send('Username not found. Please enter the name when you applied.')
+			conn.close()
 			
 		else:
 			# Needs more/less fields
 			await message.channel.send('Error: You need to add {0} fields, meaning it can only have {1} comma.'\
 				' For example: $update oldname,newname'.format(2,1))
 client.run(TOKEN)
+db.dispose()
