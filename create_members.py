aff86f9ecd2d25d7a1a061230336fc5c5c9e81f7
justingerolami <justin.gerolami@queensu.ca>
2021-08-31 00:09:07 -0400

    Added code to import previous CSV and create members table

---
+++ create_members.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+load_dotenv()
+
+# Heroku database url
+
+
+# This function creates the "members" table in the database
+# It also populates the database with previous data from the clan csv
+def setup_members_table(url,clansheetData=None):
+	engine = create_engine(url, echo = False,poolclass=NullPool)
+	engine.execute("CREATE TABLE IF NOT EXISTS members (username text PRIMARY KEY, joined timestamp)")
+	
+	if clansheetData not None:
+		clansheetData.to_sql('members', con = engine, if_exists='append',index=False)
+	
+	engine.dispose()
+
+
+# This function loads the previous data from the csv into a dataframe
+# Returns a dataframe
+def import_clansheet(filename):
+	clanData = pd.read_csv(filename)
+	date = clanData["joined"]
+
+	# Remove the time component from the old spreadsheet
+	clanData["joined"] = pd.to_datetime(date, format='%m/%d/%Y %H:%M:%S').dt.date
+	return clanData
+
+
+def main():
+	filename = "clansheet.csv"
+	url = os.getenv('url')
+	clansheet = os.getenv('clansheet')
+
+	clansheetData = import_clansheet(filename)
+	setup_members_table(url,clansheetData)
+
+
+if __name__ == '__main__':
+	main()
